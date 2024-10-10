6007d48d5856d513ecfc2a1455358aa117b4bf91
Muhammad Hisyam Kamil <hisyamkamil99@gmail.com>
2024-10-10 18:16:59 +0700

    first commit

---
+++ src/script.js
@@ -0,0 +1,29 @@
+AOS.init();
+
+// const mobileMenu = document.querySelector(".mobile-menu");
+// const openMenuBtn = document.getElementById("openMenu");
+// const closeMenuBtn = document.getElementById("closeMenu");
+
+// openMenuBtn.addEventListener("click", () => {
+//   mobileMenu.classList.remove("-translate-x-full");
+// });
+
+// closeMenuBtn.addEventListener("click", () => {
+//   mobileMenu.classList.add("-translate-x-full");
+// });
+
+// const mobileMenuItems = mobileMenu.querySelectorAll("a");
+// mobileMenuItems.forEach((item) => {
+//   item.addEventListener("click", () => {
+//     mobileMenu.classList.add("-translate-x-full");
+//   });
+// });
+
+// document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+//   anchor.addEventListener("click", function (e) {
+//     e.preventDefault();
+//     document.querySelector(this.getAttribute("href")).scrollIntoView({
+//       behavior: "smooth",
+//     });
+//   });
+// });
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: ["winter"],
+    base: true,
+    styled: true,
+    utils: true,
+    prefix: "",
+    logs: true,
+    themeRoot: ":root",
+  },
+};
